41bfd509605c059225cfc335e16120132c17e6ba
dewi08161122 <dewi08161122gmail.com>
2026-02-18 01:37:55 +0800

    電腦更換,資料轉移

--- 11.py
+++ 11.py
@@ -8,20 +8,19 @@ data = pandas.read_html(response.content, encoding='utf-8')
 
 list=data[0].iloc[5, 1]
 items = list.split(' ')
-category_list = []
-for i in items:
-    if '.' in i:
-        category=i.split('.')
-        number = category[0]
-        name = category[1]
-        category_list.append((number, name))
-print(category_list)
+
 
 with get_connection() as con:
-	with con.cursor() as cursor:
-		cursor.execute("CREATE TABLE IF NOT EXISTS stock_category(" \
-			"id BIGINT unsigned not null primary key auto_increment," \
-			"category_number varchar(255) not null," \
-			"category_name varchar(255) not null," \
-            "FOREIGN KEY (category_number) REFERENCES stock_name (category_number));"
+    with con.cursor() as cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS stock_category(" \
+            "id BIGINT unsigned not null primary key auto_increment," \
+            "category_number varchar(255) not null," \
+            "category_name varchar(255) not null," \
+            "UNIQUE KEY (category_number));"
 			)
+        for i in items:
+            if '.' in i:
+                category=i.split('.')
+                number = category[0]
+                name = category[1]
+                cursor.execute("INSERT INTO stock_category(category_number, category_name) VALUES(%s, %s,)",(number, name))
