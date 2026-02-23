a1eecbdde4b5b8a2219107477ae7d1268480014e
莊奇勳 <dewi08161122@gmail.com>
2026-02-24 00:37:46 +0800

    MVC架構優化

--- run_history.py
+++ run_history.py
@@ -1,10 +1,11 @@
 from services.twse_stock_service import TwseStock
+from services.twse_index_service import TwseIndex
 from models.stock_model import StockModel
 from utils.data_utils import FormatHelper
 import time, random
 from datetime import timedelta, date
 
-def get_TwseStock_data(start_date, end_date):
+def get_TwseStock_data(start_date, end_date):  # 資料進度 2023, 1, 1
     number_data = set(StockModel.get_all_stock_numbers())
     while start_date >= end_date:
         if start_date.weekday() >= 5:
@@ -12,28 +13,9 @@ def get_TwseStock_data(start_date, end_date):
             continue
         url=TwseStock.get_TWstock_url(start_date)
         jsondata=TwseStock.get_TWstock_json(url)
-        stock_data=TwseStock.clean_jsondata(jsondata)
-        if stock_data:
-            day_records = []
-            for i in stock_data:
-                if i[0] in number_data:
-                    change_price = FormatHelper.clean_value_to_float(i[10])
-                    if "-" in i[9] or "green" in i[9]:
-                        change_price = -abs(change_price)
-                    record = (
-                        i[0], 
-                        start_date.strftime("%Y-%m-%d"), 
-                        FormatHelper.clean_value_to_float(i[5]),
-                        FormatHelper.clean_value_to_float(i[8]),
-                        FormatHelper.clean_value_to_float(i[6]),
-                        FormatHelper.clean_value_to_float(i[7]),
-                        change_price,
-                        FormatHelper.clean_value_to_int(i[2]),
-                        FormatHelper.clean_value_to_int(i[4])
-                    )
-                    day_records.append(record)
-            
-            StockModel.save_stock_prices(day_records)
+        stock_data=TwseStock.clean_jsondata(jsondata, number_data, start_date)
+        if stock_data:          
+            StockModel.save_stock_prices(stock_data)
             print(f"[{start_date}] 儲存完成。")
         else:
             print(f"[{start_date}] 無資料 (可能是休市)。")
@@ -42,6 +24,8 @@ def get_TwseStock_data(start_date, end_date):
         sleep_time = random.uniform(5, 12) 
         time.sleep(sleep_time)
 
-# start = date(2026, 2, 1)
-# end = date(2026, 1, 1)
-# get_TwseStock_data(start, end)
+start = date(2022, 7, 1)
+end = date(2022, 6, 1)
+get_TwseStock_data(start, end)
+
+# def get_TwseIndex_data(start_date, end_date):
--- services/twse_stock_service.py
+++ services/twse_stock_service.py
@@ -1,5 +1,5 @@
-import requests, time, random, pandas
-from datetime import date, timedelta
+from utils.data_utils import FormatHelper
+import requests
 
 
 class TwseStock:
@@ -23,11 +23,32 @@ class TwseStock:
         return data
     
     @staticmethod
-    def clean_jsondata(jsondata):
-
+    def clean_jsondata(jsondata, number_data, start_date):
+        stock_data = None
         for table in jsondata.get("tables", []):
             if "每日收盤行情" in table.get('title', ''):
                 stock_data = table.get('data', [])
-                return stock_data
+                break
+        if stock_data:
+            day_records = []
+            date_str = start_date.strftime("%Y-%m-%d")
+            for i in stock_data:
+                if i[0] in number_data:
+                    change_price = FormatHelper.clean_value_to_float(i[10])
+                    if "-" in i[9] or "green" in i[9]:
+                        change_price = -abs(change_price)
+                    record = (
+                        i[0], 
+                        date_str, 
+                        FormatHelper.clean_value_to_float(i[5]),
+                        FormatHelper.clean_value_to_float(i[8]),
+                        FormatHelper.clean_value_to_float(i[6]),
+                        FormatHelper.clean_value_to_float(i[7]),
+                        change_price,
+                        FormatHelper.clean_value_to_int(i[2]),
+                        FormatHelper.clean_value_to_int(i[4])
+                    )
+                    day_records.append(record)
+            return day_records
         return None
     
