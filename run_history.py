f02579a3e4f79398f2d21241304cceb7b89f26c3
莊奇勳 <dewi08161122@gmail.com>
2026-02-25 04:34:54 +0800

    上櫃個股爬取測試完成

--- run_history.py
+++ run_history.py
@@ -1,5 +1,6 @@
 from services.twse_stock_service import TwseStock
 from services.twse_index_service import TwseIndex
+from services.tpex_stock_service import TpexStock
 from models.stock_model import StockModel
 from infrastructure.network import get_json_by_url
 import time, random
@@ -13,7 +14,7 @@ def get_TwseStock_data(start_date, end_date):  # 資料進度 2019, 7, 1
             continue
         url=TwseStock.get_TWstock_url(start_date)
         jsondata=get_json_by_url(url)
-        stock_data=TwseStock.clean_stock_data(jsondata, number_data, start_date)
+        stock_data=TwseStock.clean_TWstock_data(jsondata, number_data, start_date)
         if stock_data:          
             StockModel.save_stock_prices(stock_data)
             print(f"[{start_date}] 儲存完成。")
@@ -24,11 +25,11 @@ def get_TwseStock_data(start_date, end_date):  # 資料進度 2019, 7, 1
         sleep_time = random.uniform(5, 12) 
         time.sleep(sleep_time)
 
-# start = date(2020, 1, 1)
-# end = date(2019, 7, 1)
+# start = date(2019, 7, 1)
+# end = date(2019, 1, 1)
 # get_TwseStock_data(start, end) # 上市個股進度
 
-def get_TwseIndex_data(start_date, end_date):  # 資料進度 2024, 1, 1
+def get_TwseIndex_data(start_date, end_date):  # 資料進度 2020, 1, 1
     while start_date >= end_date:
         url_TW_k,url_TW_v=TwseIndex.get_TWindex_url(start_date)
         jsondata_TW_k=get_json_by_url(url_TW_k)
@@ -45,6 +46,28 @@ def get_TwseIndex_data(start_date, end_date):  # 資料進度 2024, 1, 1
         sleep_time = random.uniform(12, 20) 
         time.sleep(sleep_time)
 
-start = date(2025, 1, 1)
-end = date(2024, 1, 1)
-get_TwseIndex_data(start, end) # 上市大盤進度
+# start = date(2021, 1, 1)
+# end = date(2020, 1, 1)
+# get_TwseIndex_data(start, end) # 上市大盤進度
+
+def get_TpexStock_data(start_date, end_date):  # 資料進度 2026, 1, 1
+    number_data = set(StockModel.get_all_stock_numbers())
+    while start_date >= end_date:
+        if start_date.weekday() >= 5:
+            start_date -= timedelta(days=1)
+            continue
+        jsondata=TpexStock.get_tpex_data(start_date)
+        stock_data=TpexStock.clean_Tpexstock_data(jsondata, number_data, start_date)
+        if stock_data:          
+            StockModel.save_stock_prices(stock_data)
+            print(f"[{start_date}] 儲存完成。")
+        else:
+            print(f"[{start_date}] 無資料 (可能是休市)。")
+
+        start_date -= timedelta(days=1)
+        sleep_time = random.uniform(5, 12) 
+        time.sleep(sleep_time)
+
+# start = date(2026, 2, 24)
+# end = date(2026, 2, 4)
+# get_TpexStock_data(start, end) # 上櫃個股進度
---
+++ services/tpex_stock_service.py
@@ -0,0 +1,46 @@
+from utils.data_utils import FormatHelper
+import requests
+
+class TpexStock:
+    @staticmethod
+    def get_tpex_data(date):
+        url = "https://www.tpex.org.tw/www/zh-tw/afterTrading/dailyQuotes" 
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'Referer': 'https://www.tpex.org.tw/zh-tw/mainboard/after-trading/daily-close-quotes.html',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+        date_str = date.strftime("%Y/%m/%d")
+        payload = {
+            "date": date_str,
+            "id": "",
+            "response": "json"
+        }
+        session = requests.Session()
+        response = session.post(url, data=payload, headers=headers, verify=False)
+        data = response.json()
+
+        return data
+    @staticmethod
+    def clean_Tpexstock_data(jsondata, number_data, start_date):
+        stock_data = jsondata['tables'][0].get('data', [])
+        if stock_data:
+            day_records = []
+            date_str = start_date.strftime("%Y-%m-%d")
+            for i in stock_data:
+                if i[0] in number_data:              
+                    record = (
+                        i[0], 
+                        date_str, 
+                        FormatHelper.clean_value_to_float(i[4]),
+                        FormatHelper.clean_value_to_float(i[2]),
+                        FormatHelper.clean_value_to_float(i[5]),
+                        FormatHelper.clean_value_to_float(i[6]),
+                        FormatHelper.clean_value_to_float(i[3]),
+                        FormatHelper.clean_value_to_int(i[8]),
+                        FormatHelper.clean_value_to_int(i[9])
+                    )
+                    day_records.append(record)
+            return day_records
+        return None
