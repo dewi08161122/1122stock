ae08adcefa6729136a1a82482c0583c44e63de1c
莊奇勳 <dewi08161122@gmail.com>
2026-04-17 03:07:10 +0800

    製作API Documentation

--- server/controllers/member_controller.py
+++ server/controllers/member_controller.py
@@ -60,7 +60,7 @@ def login(response: Response, body: dict=Body(...)):
     except Exception as e:
         print(e)
         return{"error":True,"message":"伺服器內部錯誤"}
-@router.delete("/api/user/auth")
+@router.delete("/api/user/auth", summary="會員登出", tags=["User"])
 def logout(response: Response):
     response.delete_cookie(key="token")
     return {"ok": True}
